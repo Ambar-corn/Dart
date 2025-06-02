a2588aea063df3847774eb1fbee4e699f4d1ae90
Ambar-corn <efimsysoev2023@mail.ru>
2025-06-02 20:20:42 +0300

    sync 8

---
+++ ssync/lib/src/sync8.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+//Напишите функцию youFunction, в теле которой считывается содержимое
+//многострочного текстового файла input1.txt и ее инвертированное
+//представление записывается в output.txt .
+
+void main() {
+  void youFunction() {
+    File input1 = File('X:/DARTLESS/ssync/lib/src/input1.txt');
+    File output = File('X:/DARTLESS/ssync/lib/src/output.txt');
+
+    List<String> fileFirst = input1.readAsStringSync().split("\n");
+    List<String> invertion;
+
+    output.writeAsStringSync(
+        fileFirst.map((str) => str.split('').reversed.join('')).join('\n'));
+  }
+
+  youFunction();
+}
